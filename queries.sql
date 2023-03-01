d2892ba604c4ae892c50c31dbb4fd8a0b1219630
tobuya <thomasobuya95@gmail.com>
2023-03-01 23:01:56 +0300

    Find the maximum and minimum weight of each types of animals

--- queries.sql
+++ queries.sql
@@ -41,3 +41,4 @@ SELECT count(*) FROM animals;
 SELECT count(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
