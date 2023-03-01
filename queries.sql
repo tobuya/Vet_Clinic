e3529977375f62fd40386af390f5550e4537c8fb
tobuya <thomasobuya95@gmail.com>
2023-03-01 22:57:08 +0300

    Find average weight of the animals

--- queries.sql
+++ queries.sql
@@ -1,14 +1,14 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name = 'Luna';
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name from animalS WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name from animals WHERE neutered = true and escape_attempts < 3;
-SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
-SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
-SELECT * from animals WHERE neutered = true;
-SELECT * from animals WHERE name != 'Gabumon';
-SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+SELECT name FROM animalS WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
@@ -32,10 +32,11 @@ DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 SAVEPOINT my_savepoint;
 UPDATE animals SET weight_kg = weight_kg * -1;
 SELECT * FROM animals;
-ROLLBACK to my_savepoint;
+ROLLBACK TO my_savepoint;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 SELECT * FROM animals;
 
-SELECT count(*) from animals;
-SELECT count(*) from animals WHERE escape_attempts = 0;
+SELECT count(*) FROM animals;
+SELECT count(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
