cc6fd8c7cb8a4f81e651ab3b992b59002a6d9f61
tobuya <thomasobuya95@gmail.com>
2023-03-02 15:52:25 +0300

    Modify the inserted animals so that they contain species_id value

--- data.sql
+++ data.sql
@@ -30,3 +30,6 @@ INSERT INTO species(name)
 VALUES
 ('Pokemon'),
 ('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id= 1 WHERE species_id IS NULL;
