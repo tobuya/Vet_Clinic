dba075d5c8bef09ac21af02c39744cd62b4d33e6
tobuya <thomasobuya95@gmail.com>
2023-03-02 15:18:59 +0300

    Add owner_id column as foreign key to owners table and allow cascading on deletion

--- schema.sql
+++ schema.sql
@@ -34,3 +34,11 @@ ADD CONSTRAINT fk_species
 FOREIGN KEY (species_id)
 REFERENCES species (id)
 ON DELETE CASCADE;
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners (id)
+ON DELETE CASCADE;
