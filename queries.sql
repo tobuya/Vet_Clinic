6a5e0b7b0350a0dfa75a698a361daf5ebbf4e1f5
tobuya <thomasobuya95@gmail.com>
2023-03-02 17:34:52 +0300

    Select animals belonging to Melody Pond

--- queries.sql
+++ queries.sql
@@ -43,3 +43,5 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31' GROUP BY species;
+
+SELECT name FROM animals a JOIN owners o ON a.owner_id = o.id WHERE o.id = 4;
